90b3c9b1284d6ac590050408b5b5b102e0491697
ryan.vansickle <ryan.vansickle@trilliumeng.com>
2016-03-29 15:32:18 -0700

    SVN r2891: Checking in 64bit ProtoGen binary

--- Protogen/Linux/x64/ProtoGen.sh
+++ Protogen/Linux/x64/ProtoGen.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#/bin/sh
 appname=`basename $0 | sed s,\.sh$,,`
 
 dirname=`dirname $0`
@@ -7,6 +7,6 @@ tmp="${dirname#?}"
 if [ "${dirname%$tmp}" != "/" ]; then
 dirname=$PWD/$dirname
 fi
-LD_LIBRARY_PATH=$dirname:/usr/lib/x86_64-linux-gnu/
+LD_LIBRARY_PATH=$dirname:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
 $dirname/$appname "$@"
