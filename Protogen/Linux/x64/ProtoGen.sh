916ba8cdbb94303d6f5f2c8e0a39a43450d06ba4
ryan.vansickle <ryan.vansickle@trilliumeng.com>
2016-03-30 11:56:10 -0700

    SVN r2903: setting LD_LIBRARY_PATH to look in appropriate Ubuntu system lib dirs if Qt libs are not locall

--- Protogen/Linux/x64/ProtoGen.sh
+++ Protogen/Linux/x64/ProtoGen.sh
@@ -7,6 +7,6 @@ tmp="${dirname#?}"
 if [ "${dirname%$tmp}" != "/" ]; then
 dirname=$PWD/$dirname
 fi
-LD_LIBRARY_PATH=$dirname:$LD_LIBRARY_PATH
+LD_LIBRARY_PATH=$dirname:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
 $dirname/$appname "$@"
