179201898874b66679b17099846f2adeda30f741
jeff.fisher <jeff.fisher@trilliumeng.com>
2016-07-06 17:19:37 -0700

    SVN r3355: Prevent re-downloading of non-existent tiles

--- Examples/LineOfSight/get_tile.sh
+++ Examples/LineOfSight/get_tile.sh
@@ -4,7 +4,7 @@ FILE=$DIR/$3.terrain
 
 [ ! -d $DIR ] && mkdir -p $DIR
 
-if [ ! -f "$FILE" ]; then
+if [ ! -f "$FILE" ] && [ ! -f "$FILE.invalid" ]; then
     curl -H "Accept: application/vnd.quantized-mesh,application/octet-stream;q=0.9" http://assets.agi.com/stk-terrain/v1/tilesets/world/tiles/$1/$2/$3.terrain > $FILE.gz 2>/dev/null
-    gunzip $FILE.gz 2>/dev/null || rm $FILE.gz
+    gunzip $FILE.gz 2>/dev/null || mv $FILE.gz $FILE.invalid
 fi
