ec2fae4680eb6a2a8ed40ddf9021e0e872fe14ce
jeff.fisher <jeff.fisher@trilliumeng.com>
2017-04-14 12:42:50 -0700

    SVN r4522: Further shell compatibility improvement

--- GenerateOrionPublicPacket.sh
+++ GenerateOrionPublicPacket.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 PLATFORM=`uname -i 2>/dev/null`
-MACH=`uname -m`
 ROOT_DIR=`dirname $0`
 
 if [ "$PLATFORM" = "i386" ]; then
     ARCH=Linux/x86
 elif [ "$PLATFORM" = "x86_64" ]; then
     ARCH=Linux/x64
-elif [ "$MACH" != "${MACH/arm/}" ]; then
+elif uname -m | grep arm > /dev/null; then
     ARCH=Linux/arm
 elif [ "`uname`" = "Darwin" ]; then
     ARCH=Mac/ProtoGen.app/Contents/MacOS
