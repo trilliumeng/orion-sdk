956c781ec8719e55a7c98f3902930e7153617c03
jeff.fisher <jeff.fisher@trilliumeng.com>
2017-06-07 07:58:35 -0700

    SVN r4754: Adding a helper script for calling platform-specific ProtoGen binaries

--- GenerateOrionPublicPacket.sh
+++ GenerateOrionPublicPacket.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
 
-PLATFORM=`uname -i 2>/dev/null`
 ROOT_DIR=`dirname $0`
 
-if [ "$PLATFORM" = "i386" ]; then
-    ARCH=Linux/x86
-elif [ "$PLATFORM" = "x86_64" ]; then
-    ARCH=Linux/x64
-elif uname -m | grep arm > /dev/null; then
-    ARCH=Linux/arm
-elif [ "`uname`" = "Darwin" ]; then
-    ARCH=Mac/ProtoGen.app/Contents/MacOS
-else
-    echo "Unrecognized platform $PLATFORM"
-    exit 1
-fi
-
-$ROOT_DIR/Protogen/$ARCH/ProtoGen $ROOT_DIR/Communications/OrionPublicProtocol.xml $ROOT_DIR/Communications -no-doxygen
+$ROOT_DIR/Protogen/Protogen.sh $ROOT_DIR/Communications/OrionPublicProtocol.xml $ROOT_DIR/Communications -no-doxygen
 
---
+++ Protogen/Protogen.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+PLATFORM=`uname -i 2>/dev/null`
+ROOT_DIR=`dirname $0`
+
+if [ "$PLATFORM" = "i386" ]; then
+    ARCH=Linux/x86
+elif [ "$PLATFORM" = "x86_64" ]; then
+    ARCH=Linux/x64
+elif uname -m | grep arm > /dev/null; then
+    ARCH=Linux/arm
+elif [ "`uname`" = "Darwin" ]; then
+    ARCH=Mac/ProtoGen.app/Contents/MacOS
+else
+    echo "Unrecognized platform $PLATFORM"
+    exit 1
+fi
+
+$ROOT_DIR/$ARCH/ProtoGen $@
+
