7a767eea149d5fad07eff4a3662790bfa88df67e
jeff.fisher <jeff.fisher@trilliumeng.com>
2018-01-11 12:10:23 -0800

    SVN r5643: Fixing broken test for macOS

--- Protogen/Protogen.sh
+++ Protogen/Protogen.sh
@@ -3,14 +3,14 @@
 PLATFORM=`uname -m 2>/dev/null`
 ROOT_DIR=`dirname $0`
 
-if [ "$PLATFORM" = "i686" ]; then
+if [ "`uname`" = "Darwin" ]; then
+    ARCH=Mac/ProtoGen.app/Contents/MacOS
+elif [ "$PLATFORM" = "i686" ]; then
     ARCH=Linux/x86
 elif [ "$PLATFORM" = "x86_64" ]; then
     ARCH=Linux/x64
 elif uname -m | grep arm > /dev/null; then
     ARCH=Linux/arm
-elif [ "`uname`" = "Darwin" ]; then
-    ARCH=Mac/ProtoGen.app/Contents/MacOS
 else
     echo "Unrecognized platform $PLATFORM"
     exit 1
