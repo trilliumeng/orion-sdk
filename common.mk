b2e9ee9a1a68145202fa3524d5c7861298f991e5
jeff.fisher <jeff.fisher@trilliumeng.com>
2020-06-24 16:30:07 -0700

    SVN r9070: Conditionally assign CC and AR per #5, and add an 'arm64' target for non-tegra aarch64 builds

--- common.mk
+++ common.mk
@@ -10,10 +10,12 @@ else ifeq ($(TARGET), arm)
 	PREFIX=arm-linux-gnueabi-
 else ifeq ($(TARGET), tegra)
 	PREFIX=aarch64-unknown-linux-gnu-
+else ifeq ($(TARGET), arm64)
+	PREFIX=aarch64-linux-gnu-
 endif
 
-CC=$(PREFIX)gcc
-AR=$(PREFIX)ar
+CC?=$(PREFIX)gcc
+AR?=$(PREFIX)ar
 OBJ_DIR = $(TARGET)/obj
 
 $(shell mkdir -p $(OBJ_DIR))
